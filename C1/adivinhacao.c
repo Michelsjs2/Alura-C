506a416f1323d8d8344ff7f47c5cfc8ba2b2d1dc
Michelsjs2 <michelsjs.estudante@gmail.com>
2022-12-26 15:37:15 -0300

    acréscimo da variável com número a ser adivinhado

--- C1/adivinhacao.c
+++ C1/adivinhacao.c
@@ -6,8 +6,12 @@ int main() {
     //Código necessário para que os acentos funcionem
     setlocale(LC_ALL, "Portuguese_Brazil");
 
+    //cabeçalho do jogo
     printf("**************************************\n");
     printf("* Bem-vindo ao jogo de adivinhação! *\n");
     printf("**************************************\n");
-    return 0;
+
+    int num_secreto = 42;
+
+    printf("O número %d é o secreto. Guarde ele muito bem!", num_secreto);
 }
