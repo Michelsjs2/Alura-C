b175ab6d890e6409d903ee9eae4430a2bede2b83
Michelsjs2 <michelsjs.estudante@gmail.com>
2022-12-31 12:05:52 -0300

    utilizando mais funções

--- C2/jogo_forca.c
+++ C2/jogo_forca.c
@@ -21,13 +21,41 @@ void fpalpite(char palpites[26], int* tentativas){ //o int* é um ponteiro que ap
     (*tentativas)++;
 }
 
+//verifica se uma letra já foi palpitada
+int verifica_palpite(char letra, char palpites[26], int tentativas){
+    int acertou = 0;
+
+    for(int j = 0; j < tentativas; j++){
+        if(palpites[j] == letra){
+            acertou = 1;
+            break;
+        }
+    }
+
+    return acertou;
+}
+
+//imprime a palavra secreta
+void desenha_forca(char palavra_secreta[20], char palpites[26], int tentativas){
+    for (int i = 0; i < strlen(palavra_secreta); i++){ 
+
+        int acertou = verifica_palpite(palavra_secreta[i], palpites, tentativas);
+        if(acertou){
+            printf("%c ", palavra_secreta[i]);
+        } else {
+            printf("_ ");
+        }
+    }
+    printf("\n\n");
+}
+
+void escolhe_palavra(char palavra_secreta[20]){
+    sprintf(palavra_secreta, "TESTANDO"); //esse comando facilita a inserção de uma palavra dentro de um array de caracteres
+}
+
 int main(){
     setlocale(LC_ALL, "Portuguese_Brazil");
-    
-    iniciar_jogo();
-
     char palavra_secreta [20]; //uma variável que possui um [] no final de um array
-    sprintf(palavra_secreta, "TESTANDO"); //esse comando facilita a inserção de uma palavra dentro de um array de caracteres
     
     /*array que guarda os palpites do usuário e int que 
     verifica o número de vezes que o usuário deu palpites*/
@@ -37,29 +65,12 @@ int main(){
     int vitoria = 0;
     int forca = 0;
 
+    escolhe_palavra(palavra_secreta);
+    iniciar_jogo();
+
     do {
-        //imprime a palavra secreta
-        for (int i = 0; i < strlen(palavra_secreta); i++){ 
 
-            int acertou = 0;
-
-            //verifica se uma letra já foi palpitada
-            for(int j = 0; j < num_tentativas; j++){
-                if(tentativa_usuario[j] == palavra_secreta[i]){
-                    acertou = 1;
-                    break;
-                }
-            }
-            
-            if(acertou){
-                printf("%c ", palavra_secreta[i]);
-            } else {
-                printf("_ ");
-            }
-        }
-
-        printf("\n\n");
-        
+        desenha_forca(palavra_secreta, tentativa_usuario, num_tentativas);
         fpalpite(tentativa_usuario, &num_tentativas);
 
     } while(!vitoria && !forca);
