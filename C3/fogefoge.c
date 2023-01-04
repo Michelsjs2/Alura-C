00f2250ea6d1a0f323187800ce2b08f919bded1d
Michelsjs2 <michelsjs.estudante@gmail.com>
2023-01-04 20:09:12 -0300

    alocando memória para gerar mapa dinâmico

--- C3/fogefoge.c
+++ C3/fogefoge.c
@@ -1,15 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main (){
-    //matriz de 5 por 10
-    char mapa[5][10+1];
+//matriz de 5 por 10
+char** mapa;
+int linhas;
+int colunas;
 
+int main (){
     FILE* f;
     f = fopen("mapa.txt", "r");
     if (f == 0){
         printf("Erro ao na leitura do arquivo\n");
         exit(1);
+    }
+
+    fscanf (f, "%d %d", &linhas, &colunas);
+
+    //alocando espaço na memória de forma dinâmica
+    mapa = malloc(sizeof(char*) * linhas);
+    for (int i = 0; i < linhas; i++){
+        mapa[i] = malloc(sizeof(char) * (colunas +1));
     }
 
     //escaneia o arquivo linha a linha
@@ -22,4 +32,10 @@ int main (){
     }
 
     fclose(f);
+
+    //liberando o espaço alocado na memória
+    for (int i = 0; i < linhas; i++){
+        free(mapa[i]);
+    }
+    free(mapa);
 }
